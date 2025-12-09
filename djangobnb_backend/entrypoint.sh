a495d1864a1dd1649994c2a2659c1a0c8c21e2af
unknown <reymargutana46.com>
2025-12-09 22:28:51 +0800

    Updating Set up docker for django with postgresql, etc - Gutana, Reymar C

---
+++ djangobnb_backend/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres"]
+    echo "Check if database is running..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+        sleep 0.1
+    done
+
+    echo "The database is up and running :-D"
+fi
+
+python manage.py migrate
+
+exec "$@"
